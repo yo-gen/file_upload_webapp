1e7ed9fce9f5aed138e8b9f910aec1a584be223f
Yogen Aggarwal <yogenaggarwal@Yogens-MacBook-Air.local>
2022-04-06 22:14:05 +0530

    Add the File uploads Model and connfigure basic active storage

---
+++ app/models/upload.rb
@@ -0,0 +1,5 @@
+class Upload < ApplicationRecord
+  belongs_to :user
+
+  has_one_attached :file
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_06_162007) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_06_162201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_06_162007) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "uploads", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_uploads_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -56,4 +65,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_06_162007) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "uploads", "users"
 end
