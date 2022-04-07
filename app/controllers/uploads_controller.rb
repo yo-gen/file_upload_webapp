07c7317c77c9d3f41996d9880c541ca322e64b3f
Yogen Aggarwal <yogenaggarwal@Yogens-MacBook-Air.local>
2022-04-07 05:42:58 +0530

    Add file upload form and basic file upload functionality. Cleaned the Navbar.

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -6,6 +6,19 @@ class UploadsController < ApplicationController
   end
 
   def new
-    @upload = Upload.new
+    @upload = current_user.uploads.new
+  end
+
+  def create
+    @upload = current_user.uploads.new(upload_params)
+    @upload.file.attach(upload_params[:file])
+    @upload.save!
+    redirect_to root_path
+  end
+
+  private
+
+  def upload_params
+    params.require(:upload).permit(:title, :description, :file)
   end
 end
