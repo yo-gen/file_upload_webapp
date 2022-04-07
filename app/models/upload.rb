a6a8d1d6f0c6c1a8024e23fefc747af80dafe405
Yogen Aggarwal <yogenaggarwal@Yogens-MacBook-Air.local>
2022-04-08 01:22:19 +0530

    Add Compression for Files above 50MB

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,8 +1,25 @@
 class Upload < ApplicationRecord
-  belongs_to :user
+  after_create_commit :compress_big_files
 
+  belongs_to :user
   has_one :tiny_link, dependent: :destroy
 
   has_one_attached :file
   validates :file, size: { less_than: 1.gigabytes, message: 'File is too large' }
+
+  def compress_big_files
+    return if file.content_type == "application/zip"
+    return if file.byte_size <= 52428800 # Compress only if file size greater than 50 MB
+    zipped_filename = "#{file.filename.to_s}.gz"
+    tempfile = Tempfile.new()
+    begin
+      Zlib::GzipWriter.open(tempfile.path) do |gz|
+        gz.write(file.download)
+      end
+      file.attach(io: File.open(tempfile.path), filename: zipped_filename, content_type: 'application/zip')
+    ensure
+      tempfile.close
+      tempfile.unlink
+    end
+  end
 end
